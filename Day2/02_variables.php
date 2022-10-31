d6c29a115a153e495bfb845ff7102402723ee200
Michael Ndula <michaelndula@gmail.com>
2022-10-31 22:07:47 +0300

    PHP variables

---
+++ Day2/02_variables.php
@@ -0,0 +1,36 @@
+<?php
+/*---------PHP Data Types --------*/
+/*
+    - String: series of characters surrounded by quotes.
+    - Integer: Whole numbers
+    - Float: Decimal numbers.
+    - Boolean: True or False.
+    - Array: Special variable, which can hold more than one value.
+    - Object: A class.
+    - NULL: Empty variable.
+    - Resource: Special variable that holds a resource.
+*/
+
+/*------- Variable Rules --------- */
+/**
+ * - Variables must be prefixed with '$'
+ * - Variables must start with a letter or the underscore Character
+ * - Variables can't start with a number
+ * - variables can only contain alpha-numeric character amd underscores (A-z, 0-9. amd _)
+ * - variables are case-sensitive ($name and #NAME are two different variables)
+ */
+
+ //strings
+ $string1 = "Hello ";
+ $string2 = 'World';
+
+ echo $string1.$string2;
+
+ echo '<br/>';
+ echo '<br/>';
+ 
+ //integers
+ $int1 = 42;
+ $int2 = -20;
+
+ echo $int1 + $int2;
