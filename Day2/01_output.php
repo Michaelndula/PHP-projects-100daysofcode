2a3471d6f927b8b4414380c6088b27030d5cb97f
Michael Ndula <michaelndula@gmail.com>
2022-10-31 21:47:28 +0300

    output commands in php

---
+++ Day2/01_output.php
@@ -0,0 +1,20 @@
+<?php
+//echo command: outputs string, numbers, htm e.t.c
+ echo 'Hello';
+
+ echo '<br/>';
+ echo '<br/>';
+ //print - works the same as echo but can only output single arguments
+ print 123;
+
+ echo '<br/>';
+ echo '<br/>';
+ //print_r() - print single values and arrays
+ print_r([1,2,3,4]);
+
+ //var_dump() -  Returns more info like data type and length
+ var_dump('Hello');
+
+ //var_export() - similar to var_dump. outputs a string representation of a variable
+ var_export('hello');
+?>
