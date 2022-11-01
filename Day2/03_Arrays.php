d6fb3ca3450cbb7c63ee6c67217ae14a90275c2a
Michael Ndula <michaelndula@gmail.com>
2022-11-01 05:04:53 +0300

    Worked with Assotiative Arrays and how to access it's members

--- Day2/03_Arrays.php
+++ Day2/03_Arrays.php
@@ -1,11 +1,29 @@
 <?php
 //simple Array objects
-$numbers = [1,44,67,20];
-$fruits = array('Mango', 'Orange', 'Banana', 'Pineapple');
+// $numbers = [1,44,67,20];
+// $fruits = array('Mango', 'Orange', 'Banana', 'Pineapple');
 
-print_r($fruits);
-var_dump($fruits);
+// print_r($fruits);
+// var_dump($fruits);
 
-//output a specific fruit from the Array object
-echo $fruits[2];
+// //output a specific fruit from the Array object
+// echo $fruits[2];
 
+//Assotiative arrays
+$colors = [
+    1 => 'red',
+    4 => 'green',
+    6 => 'yellow'
+];
+
+// echo $colors[4];
+
+$person = [
+ 'first_name' => 'Michael',
+ 'last_name' => 'Ndula',
+ 'email' => 'michael@gmail.com'
+];
+
+// echo $person['first_name'];
+
+//Multi_dimensional Arrays
