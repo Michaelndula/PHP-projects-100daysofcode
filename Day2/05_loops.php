c253bb5950d885e41b6573cd00ce02087a656f0e
Michael Ndula <michaelndula@gmail.com>
2022-11-01 06:01:09 +0300

    working with loops

---
+++ Day2/05_loops.php
@@ -0,0 +1,13 @@
+<?php
+//loops executes code when a particular condition is methods
+/**------- For Loop --------- */
+/*
+ ** For Loop syntax
+ for(initialize; condition; increment){
+   //code to be executed
+}
+*/
+
+for ($i=0; $i <= 10; $i++) { 
+    echo $i;
+}
