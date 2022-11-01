9c8bff9cdcf048bbfd2c5a7cb509bb7f0bc09561
Michael Ndula <michaelndula@gmail.com>
2022-11-01 05:26:24 +0300

    Learned about conditionals in php

---
+++ Day2/04_conditionals.php
@@ -0,0 +1,25 @@
+<?php
+//conditionals are controlled structures
+/**
+ * < Less than
+ * > Greater than
+ * <= Less than ot equal to
+ * >= Greater than or equal to
+ * == Equal to
+ * === Identical to
+ * != Not equal to
+ * !== Not identical to
+ * 
+ * * If statement syntax
+ * if(condition){
+ *  // code to be executed
+ * }
+ */
+
+ $age = 16;
+
+ if ($age >= 18){
+    echo 'Old enough to VOTE';
+ }else{
+    echo 'Sorry you are not allowed to vote';
+ }
