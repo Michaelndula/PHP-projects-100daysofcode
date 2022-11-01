4d8f0cce6fac1a6fd9dbdd48fdf83e469fe7afdc
Michael Ndula <michaelndula@gmail.com>
2022-11-01 04:56:50 +0300

    Output a specific member of the Array added

--- Day2/03_Arrays.php
+++ Day2/03_Arrays.php
@@ -3,5 +3,9 @@
 $numbers = [1,44,67,20];
 $fruits = array('Mango', 'Orange', 'Banana', 'Pineapple');
 
-print_r($numbers);
-var_dump($numbers);
+print_r($fruits);
+var_dump($fruits);
+
+//output a specific fruit from the Array object
+echo $fruits[2];
+
