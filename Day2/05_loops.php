e4b7f1e02d52cc9108ec6937b0af62cbaec91ad8
Michael Ndula <michaelndula@gmail.com>
2022-11-01 06:09:44 +0300

    working with while loop

--- Day2/05_loops.php
+++ Day2/05_loops.php
@@ -8,6 +8,21 @@
 }
 */
 
-for ($i=0; $i <= 10; $i++) { 
-    echo 'Number ' . $i . '<br>';
+// for ($i=0; $i <= 10; $i++) { 
+//     echo 'Number ' . $i . '<br>';
+// }
+
+
+/**------- While Loop --------- */
+/*
+ ** While Loop syntax
+ while(condition){
+   //code to be executed
+}
+*/
+
+$int = 1;
+while ($int <= 20) {
+    echo 'int ' . $int . '<br>';
+    $int++;
 }
