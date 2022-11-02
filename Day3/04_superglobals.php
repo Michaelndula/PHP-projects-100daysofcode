369f34204fa0414ab3e1b09f39ef62fed5765371
Michael Ndula <michaelndula@gmail.com>
2022-11-02 06:29:38 +0300

    learned about super globals

---
+++ Day3/04_superglobals.php
@@ -0,0 +1,15 @@
+<?php
+//Some predefined variables in PHP are "superglobals", which means that they are always accessible, regardless of scope - and you can access them from any function, class or file without having to do anything special.
+/*
+The PHP superglobal variables are:
+
+$GLOBALS
+$_SERVER - contains information about the server environment.
+$_REQUEST - containf information about variables passed through the form or url
+$_POST - contains information about variables passed through a form
+$_GET - contains information about variables passed through a url or a form
+$_FILES - contains information about files uploaded to script
+$_ENV - contains information about the environment variable
+$_COOKIE - containd information about variables passed through a cookie
+$_SESSION - contains information about variables passed through a session
+*/
