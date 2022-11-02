5f200f304919117a843d5c0e325db2d3e7542317
Michael Ndula <michaelndula@gmail.com>
2022-11-02 04:43:01 +0300

    learning about functions

---
+++ Day3/01_functions.php
@@ -0,0 +1,8 @@
+<?php
+//function to register a user
+/**Create a function called registerUser() that will have an argument $email*/
+function registerUser($email) {
+    echo $email . ' registered';
+}
+//call the function passing in a parameter
+registerUser('Michael Ndula');
