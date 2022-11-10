86e58e9d0789a02069b3343d04f4167c1bf5d20c
Michael Ndula <michaelndula@gmail.com>
2022-11-10 04:43:52 +0300

    learnong about exceptions

--- Day6/01_exceptions.php
+++ Day6/01_exceptions.php
@@ -8,3 +8,20 @@
     exceptions. Each try must have at least one corresponding catch or finally block.
  */
 
+function inverse($x){
+    if(!$x){
+        throw new Exception('Division by 0');
+    }
+    return 1/$x;
+}
+
+echo inverse(2);
+
+try{
+    echo inverse(5);
+    echo inverse(0);
+}catch(Exception $e){
+    echo 'Caught Exception ', $e->getMessage(), ' ';
+}finally{
+    echo 'First Finally';
+}
