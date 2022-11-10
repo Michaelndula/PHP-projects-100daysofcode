12faff93325d47445bb3666fa6a4387d234bd534
Michael Ndula <michaelndula@gmail.com>
2022-11-10 04:34:03 +0300

    learnong about exceptions

---
+++ Day6/01_exceptions.php
@@ -0,0 +1,10 @@
+<?php
+/*----------Exceptions-----------*/
+
+/*
+    PHP has exceptions model similar to that of other programming languages. 
+    An exception can be thrown, and caught ("catched") Within PHP.
+    Code may be surrounded in a try block, to facilitate the catching of potential
+    exceptions. Each try must have at least one corresponding catch or finally block.
+ */
+
