2b58696a69434f459afc585848fff4c8b02e9508
Michael Ndula <michaelndula@gmail.com>
2022-11-22 20:01:18 +0300

    The __constructor function

--- OOP-Day3-Practice/problem-set-1.php
+++ OOP-Day3-Practice/problem-set-1.php
@@ -6,7 +6,7 @@
 
     class Myclass{
         public function __construct(){
-            echo 'MyClass class has initialized !';
+            echo 'A constructor allows you to initialize an object\'s properties upon creation of the object. If you create a __construct() function, PHP will automatically call this function when you create an object from a class.';
         }
     }
     $output = new Myclass;
