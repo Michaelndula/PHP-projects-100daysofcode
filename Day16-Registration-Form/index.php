e0cba24373f761271ab6e79df242cff3aca78456
Michael Ndula <michaelndula@gmail.com>
2022-12-07 20:19:49 +0300

    updated the user regiseration form

--- Day16-Registration-Form/index.php
+++ Day16-Registration-Form/index.php
@@ -1,28 +1,14 @@
 <?php
     include 'header.php';
+    require_once ('config.php');
+
 	// define variables and set them to empty string
 	$first_name = $last_name = $email = $city = $country = $password = "";
-
-	if(isset($_POST['registerBtn'])){
-		$first_name = $_POST['first_name'];
-		$last_name = $_POST['last_name'];
-		$email = $_POST['email'];
-		$city = $_POST['city'];
-		$country = $_POST['country'];
-		$password = $_POST['password'];
-	}
-
-	function test_input($data){
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
 
 ?>
 
 <div class="signup-form">
-    <form action="<?php echo htmlspecialchars('registration.php');?>" class="form" method="post">
+    <form action="<?php echo htmlspecialchars('index.php');?>" class="form" method="post">
 		<h2>Sign Up</h2>
 		<p>Please fill in this form to create an account!</p>
 		<div class="form-group">
