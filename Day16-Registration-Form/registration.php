fa98aace602b6b74b4b1390a4174671bb1b8ff0a
Michael Ndula <michaelndula@gmail.com>
2022-12-07 20:12:46 +0300

    succesfully sends data to the db

--- Day16-Registration-Form/registration.php
+++ Day16-Registration-Form/registration.php
@@ -1,19 +1,50 @@
+<?php
+ require_once ('config.php');
+?>
 <html>
     <body>
-     <h2>Hello  <?php echo $_POST['first_name'];?>  Nice to see you</h2>
+     <h2>Hello  <?php echo $_REQUEST['first_name'];?>  Nice to see you</h2>
      <h4>Displayed are you inputs:</h4>
      <p>
     <?php
         $first_name = $last_name = $email = $city = $country = $password = "";
-        echo 'Full Name: '. $_POST['first_name'] . " ". $_POST['last_name'];
-        echo "<br>";
-        echo 'E-mail: '. $_POST['email'];
-        echo "<br>";
-        echo 'City: ' . $_POST['city'];
-        echo "<br>";
-        echo 'Country: ' . $_POST['country'];
-        echo "<br>";
-        echo 'Password: ' . $_POST['password'];
+        
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+		//check connection status
+		if($conn === false){
+			die("Error: could not connect to server" . mysqli_connect_error());
+		}
+		if(isset($_POST['registerBtn'])){
+		//Take the form input data
+		$id = "";
+		$first_name = $_REQUEST['first_name'];
+		$last_name = $_REQUEST['last_name'];
+		$email = $_REQUEST['email'];
+		$city = $_REQUEST['city'];
+		$country = $_REQUEST['country'];
+		$password = $_REQUEST['password'];
+
+		//insert the data into our db
+		$sql = "INSERT INTO users VALUES ('$id', '$first_name', '$last_name', '$email', '$city', '$country', '$password')";
+
+		//check if query is successful
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+			. " Please browse your localhost php my admin"
+			. " to view the updated data</h3>";
+
+			echo nl2br("\n$first_name\n $last_name\n "
+			. "$email\n $city\n $country\n $password");
+		}else{
+			echo "ERROR: Hush! Sorry $sql. "
+			. mysqli_error($conn);
+		}
+	}
+
+		//close connection
+		mysqli_close($conn);
+        
     ?>
     </p>
     </body>
