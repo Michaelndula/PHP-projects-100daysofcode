00241d0d9f1cadf696beac47fdc9c09056e0b4ad
Michael Ndula <michaelndula@gmail.com>
2022-12-07 19:01:08 +0300

    solving the errors in the form

--- Day16-Registration-Form/registration.php
+++ Day16-Registration-Form/registration.php
@@ -1,99 +1,34 @@
 <?php
  require_once ('confirmation.php');
+ include 'header.php';
+
+ // define variables and set to empty values
+$nameErr = $emailErr = $city = $county = $passwordErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["first_name"])) {
+	   $nameErr = "Name is required";
+   } else {
+	   $name = test_input($_POST["first_name"]);
+	   // check if name only contains letters and whitespace
+	   if (!preg_match("/^[a-zA-Z-' ]*$/",$first_name)) {
+		   $nameErr = "Only letters and white space allowed";
+	   }
+   }
+}
+// form validation: ensure that the form is correctly filled ...
+ // by adding (array_push()) corresponding error unto $errors array
+ if (empty($first_name)) { array_push($error_msg, "first_name is required"); }
+ if (empty($last_name)) { array_push($error_msg, "last_name is required"); }
+ if (empty($email)) { array_push($error_msg, "email is required"); }
+ if (empty($city)) { array_push($error_msg, "city is required"); }
+ if (empty($country)) { array_push($error_msg, "country is required"); }
+ if (empty($password)) { array_push($error_msg, "Password is required"); }
+ if ($password != $confirm_password) {
+   array_push($error_msg, "The two passwords do not match");
+ }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-<title>Bootstrap Simple Login Form with Blue Background</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-<style>
-	body {
-		color: #fff;
-		background: #3598dc;
-		font-family: 'Roboto', sans-serif;
-	}
-    .form-control{
-		height: 41px;
-		background: #f2f2f2;
-		box-shadow: none !important;
-		border: none;
-	}
-	.form-control:focus{
-		background: #e2e2e2;
-	}
-    .form-control, .btn{        
-        border-radius: 3px;
-    }
-	.signup-form{
-		width: 390px;
-		margin: 30px auto;
-	}
-	.signup-form form{
-		color: #999;
-		border-radius: 3px;
-    	margin-bottom: 15px;
-        background: #fff;
-        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-        padding: 30px;
-    }
-	.signup-form h2 {
-		color: #333;
-		font-weight: bold;
-        margin-top: 0;
-    }
-    .signup-form hr {
-        margin: 0 -30px 20px;
-    }    
-	.signup-form .form-group{
-		margin-bottom: 20px;
-	}
-	.signup-form input[type="checkbox"]{
-		margin-top: 3px;
-	}
-	.signup-form .row div:first-child{
-		padding-right: 10px;
-	}
-	.signup-form .row div:last-child{
-		padding-left: 10px;
-	}
-    .signup-form .btn{        
-        font-size: 16px;
-        font-weight: bold;
-		background: #3598dc;
-		border: none;
-		min-width: 140px;
-    }
-	.signup-form .btn:hover, .signup-form .btn:focus{
-		background: #2389cd !important;
-        outline: none;
-	}
-    .signup-form a{
-		color: #fff;
-		text-decoration: underline;
-	}
-	.signup-form a:hover{
-		text-decoration: none;
-	}
-	.signup-form form a{
-		color: #3598dc;
-		text-decoration: none;
-	}	
-	.signup-form form a:hover{
-		text-decoration: underline;
-	}
-    .signup-form .hint-text {
-		padding-bottom: 15px;
-		text-align: center;
-    }
-</style>
-</head>
-<body>
+
 <div class="signup-form">
     <form action="registration.php" class="form" method="POST">
 		<h2>Sign Up</h2>
@@ -112,32 +47,33 @@
         </div>
         <div class="form-group">
 			<div class="row">
-				<div class="col-xs-6"><input type="text" class="form-control" name="first_name" placeholder="First Name" autocomplete="off" required="required"></div>
-				<div class="col-xs-6"><input type="text" class="form-control" name="last_name" placeholder="Last Name" autocomplete="off" required="required"></div>
+				<div class="col-xs-6"><input type="text" class="form-control" name="first_name" placeholder="First Name" autocomplete="off"></div>
+				<div class="col-xs-6"><input type="text" class="form-control" name="last_name" placeholder="Last Name" autocomplete="off"></div>
 			</div>        	
         </div>
         <div class="form-group">
-            <input type="email" class="form-control" name="email" placeholder="Email" autocomplete="off" required="required">
+            <input type="email" class="form-control" name="email" placeholder="Email" autocomplete="off">
+			<span><?php echo $error_msg ?></span>
         </div>
 
         <div class="form-group">
         <div class="row">
-            <div class="col-xs-6"><input type="text" class="form-control" name="city" placeholder="City" required="required"></div>
-            <div class="col-xs-6"><input type="country" class="form-control" name="country" placeholder="Country" required="required"></div>
+            <div class="col-xs-6"><input type="text" class="form-control" name="city" placeholder="City"></div>
+            <div class="col-xs-6"><input type="country" class="form-control" name="country" placeholder="Country"></div>
         </div>
         </div> 
 		
 		<div class="form-group">
-            <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="off" required="required" >
+            <input type="password" class="form-control" name="password" placeholder="Password" autocomplete="off" >
         </div>  
         <div class="form-group">
 		<p>password must be at least 5 characters and<br /> have a special character, e.g. !#$.,:;()</font></p>
 	    </div>	      
 		<div class="form-group">
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" autocomplete="off" required="required">
+            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" autocomplete="off">
         </div>        
         <div class="form-group">
-			<label class="checkbox-inline"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
+			<label class="checkbox-inline"><input type="checkbox"> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
 		</div>
 		<div class="form-group">
             <button type="submit" name="registerBtn" class="btn btn-primary btn-lg">Sign Up</button>
