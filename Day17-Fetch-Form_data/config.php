cc4fae8a23df434977db27d2ac84ad0054c7bab2
Michael Ndula <michaelndula@gmail.com>
2022-12-08 19:36:57 +0300

    connected my db succesfully

--- Day17-Fetch-Form_data/config.php
+++ Day17-Fetch-Form_data/config.php
@@ -1,3 +1,15 @@
 <?php
-    
+    //initialize db variables
+    $db_server = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'register';
+
+    //create connection
+    $conn = new mysqli($db_server, $db_user, $db_pass, $db_name);
+
+    //check connection status
+    if ($conn->connect_error){
+        die('Connection error: ' . $conn->connect_error);
+    }
 ?>
